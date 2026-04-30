717a55cd51c8f86dca21f2d9db62aa6c077d2ba9
James Harton <james@harton.dev>
2026-05-01 09:47:58 +1200

    chore: add devcontainer setup

---
+++ .devcontainer/setup.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+if [ -e .asdf-plugins ]; then
+  source .asdf-plugins
+fi
+
+if [ ! -e .tool-versions ]; then
+  echo "No .tool-versions file found!"
+  exit 1
+fi
+
+set -euo pipefail
+
+PATH="${ASDF_DATA_DIR:-$HOME/.asdf}/shims:$PATH"
+REQUIRED_PLUGINS=$(cat .tool-versions | cut -d \  -f 1)
+INSTALLED_PLUGINS=$(asdf plugin list || echo "")
+
+echo -n "==> Installing/updating required plugins: "
+for PLUGIN in $REQUIRED_PLUGINS; do
+  if [[ $INSTALLED_PLUGINS =~ (^|[[:space:]])"$PLUGIN"($|[[:space:]]) ]]; then
+    asdf plugin update $PLUGIN
+  else
+    asdf plugin add $PLUGIN
+  fi
+  echo -n "$PLUGIN "
+done
+echo
+
+echo "==> Installing tools: "
+asdf install
+
+echo "==> Setting up Hex and Rebar"
+mix local.hex --force
+mix local.rebar --force
+
+echo "==> Installing nerves_bootstrap"
+mix archive.install hex nerves_bootstrap --force
+
+echo "==> Setup complete!"
